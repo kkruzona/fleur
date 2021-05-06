c25b371a918ee8c27442dc584b9976f705c6ccac
Kelsey Kruzona <kelsey.herlein@gmail.com>
2021-05-05 22:00:10 -0400

    cart successful progress

--- cart.js
+++ cart.js
@@ -1,5 +1,52 @@
-let products = localStorage.getItem('cart') ?
-              JSON.parse(localStorage.getItem('cart')): 
-              [];
-              console.log(products);
+function addToCart() {
+    let products = localStorage.getItem('cart') ?
+    JSON.parse(localStorage.getItem('cart')): 
+    [];
+    console.log(products);
 
+    for(let product of products){
+        createDiv(product);
+    }
+}
+
+function createDiv(product) {    
+
+let item = `<div id="cartListing">
+<a href=""><img src="${product.image}" class="cartImage"></a>
+<div id="cartOverview">
+    <div id="cartDetails">
+        <h3>${product.category}</h3>
+        <h2>${product.name}</h2>
+    </div>
+        <h2 id="price">$${product.price}</h2>
+</div>`
+document.getElementById("cartResults").innerHTML+=item
+}
+addToCart();
+
+let total = 0;
+let totalSpan = document.getElementById("total");
+function updateTotal(value){
+    total = (total + value);
+    if (total < 0) {
+        total = 0;
+    }
+    totalSpan.innerHTML = total.toFixed(2);
+}
+updateTotal();
+
+
+
+// ${product.price}.value
+
+
+// window.localStorage.clear();
+// window.localStorage.removeItem('name');
+
+
+// var divElement = document.createElement("Div");
+// divElement.id = "cartListing";
+
+
+// // Appending the div element to cart div
+// document.getElementsById("cartResults")[0].appendChild(divElement);
--- script.js
+++ script.js
@@ -1,41 +1,61 @@
 let products = [
     {
+        image: "Aloe-Plant.jpg",
+        category: "Succulent",
         name:'Aloe Plant', 
-        price: 13.50,
+        price: 13.95,
     },
     {
+        image: "Cactus-Plant.jpg",
+        category: "Succulent",
         name:'Cactus Plant', 
         price: 12.95,
     },
     {
-        name:"Designer's Choice Bouquet", 
+        image: "Designers-Choice-Bouquet.jpg",
+        category: "Arrangement",
+        name:"Designer's Choice", 
         price: 29.99,
     },
     {
+        image: "Eucalyptus-Bundle.jpg",
+        category: "Single Stem",
         name:'Eucalyptus Bundle', 
         price: 14.99,
     },
     {
+        image: "Lilies.jpg",
+        category: "Single Stem",
         name:'Lilies', 
         price: 18.99,
     },
     {
+        image: "Orchids.jpg",
+        category: "House Plant",
         name:'Orchids', 
         price: 24.99,
     },
     {
+        image: "Peonies.jpg",
+        category: "Single Stem",
         name:'Peonies', 
         price: 16.95,
     },
     {
+        image: "Rose-Bouquet.jpg",
+        category: "Arrangement",
         name:'Rose Bouquet', 
         price: 39.99,
     },
     {
+        image: "Succulent-Planter.jpg",
+        category: "Succulent",
         name:'Succulent Planter', 
         price: 45.99,
     },
     {
+        image: "Sunflower-Bouquet.jpg",
+        category: "Arrangement",
         name:'Sunflower Bouquet', 
         price: 18.99,
     },
