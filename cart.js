13f2361ba67303d20003e047749c5a775c21b1d9
Kelsey Kruzona <kelsey.herlein@gmail.com>
2021-05-06 21:10:36 -0400

    almost done

--- cart.js
+++ cart.js
@@ -15,25 +15,49 @@ let item = `<div id="cartListing">
 <a href=""><img src="${product.image}" class="cartImage"></a>
 <div id="cartOverview">
     <div id="cartDetails">
-        <h3>${product.category}</h3>
+        <h3 class="category">${product.category}</h3>
         <h2>${product.name}</h2>
     </div>
-        <h2 id="price">$${product.price}</h2>
+        <h2 id="price" name="price">$${product.price}</h2>
 </div>`
 document.getElementById("cartResults").innerHTML+=item
 }
 addToCart();
 
-let total = 0;
-let totalSpan = document.getElementById("total");
-function updateTotal(value){
-    total = (total + value);
-    if (total < 0) {
-        total = 0;
+function runningTotal() {
+    // let taxRate = 0.06;
+    // let tax = subtotal * taxRate;
+    // let total = subtotal + tax;
+    let cart = localStorage.getItem('cart') ?
+    JSON.parse(localStorage.getItem('cart')): 
+    [];
+    let price = document.getElementsByName("price");
+    let total = 0;
+    for (let i = 0; i < cart.length; i++) {
+      total += parseInt(cart[i].price);
     }
-    totalSpan.innerHTML = total.toFixed(2);
-}
-updateTotal();
+    // document.getElementById("total").innerHTML = total;
+    document.getElementById('subtotal').innerHTML="Sub-total: $"+total.toFixed(2);      
+    document.getElementById('taxes').innerHTML="Taxes: $"+(total*.06).toFixed(2);      
+    document.getElementById('grandtotal').innerHTML="Grand-total: $"+(total*1.06).toFixed(2);
+  }
+runningTotal();  
+
+
+//   function subTotal() {}
+
+
+
+// let total = 0;
+// let totalSpan = document.getElementById("total");
+// function updateTotal(value){
+//     total = (total + value);
+//     if (total < 0) {
+//         total = 0;
+//     }
+//     totalSpan.innerHTML = total.toFixed(2);
+// }
+// updateTotal();
 
 
 
